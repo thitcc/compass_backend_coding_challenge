5bef062ecf543a5213a26ba2331032c2a27ee4dd
Thiago Costa <thitccp@gmail.com>
2025-05-08 17:16:02 -0300

    feat: add weekday to serializer

--- app/serializers/api/v1/time_allocation_serializer.rb
+++ app/serializers/api/v1/time_allocation_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::TimeAllocationSerializer < ActiveModel::Serializer
-  attributes :location, :activity, :date
+  attributes :location, :activity, :date, :weekday
 
   def location
     object.location&.name
@@ -7,5 +7,9 @@ class Api::V1::TimeAllocationSerializer < ActiveModel::Serializer
 
   def date
     object.formatted_date
+  end
+
+  def weekday
+    object.date.strftime('%A')[0,3].upcase
   end
 end
