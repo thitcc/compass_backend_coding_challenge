52a50e36c92724b1fd577d90ca264ee10bbbac1f
Thiago Costa <thitccp@gmail.com>
2025-05-08 01:49:16 -0300

    gemfile: configure annotate

--- app/models/time_allocation.rb
+++ app/models/time_allocation.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: time_allocations
+#
+#  id          :integer          not null, primary key
+#  activity    :string
+#  date        :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  location_id :integer          not null
+#  user_id     :integer          not null
+#
+# Indexes
+#
+#  index_time_allocations_on_location_id  (location_id)
+#  index_time_allocations_on_user_id      (user_id)
+#
+# Foreign Keys
+#
+#  location_id  (location_id => locations.id)
+#  user_id      (user_id => users.id)
+#
 class TimeAllocation < ApplicationRecord
   belongs_to :user
   belongs_to :location
